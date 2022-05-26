25cabf4f13a7bcfbc0c7d3210cb21250b02dde63
Nicholas Galipi <nicholas.gomes@nfe.io>
2022-05-26 14:26:50 -0300

    fixed CORS errors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const roomsRouter = require('./routes/rooms')
 const connectDB = require('./config/db')
+var cors = require('cors')
 
 
 connectDB();
 
 const app = express()
-const port = 3000
+const port = 3001
+app.use(cors())
 app.use('/rooms', roomsRouter)
 
 app.get('/', (req, res) => {
